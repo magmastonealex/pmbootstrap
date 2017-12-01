41e9214927ca75b128582ec3320b49a62c230d62
Alex Roth <alex@magmastone.net>
2017-11-30 22:06:39 -0500

    Added a lot more Luna packages. It is now possible to start Luna, complete the first use app, and use the system. ARM support still broken.

--- aports/main/postmarketos-ui-luna/start_luna.sh
+++ aports/main/postmarketos-ui-luna/start_luna.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-if test -z "${XDG_RUNTIME_DIR}"; then
-	export XDG_RUNTIME_DIR=/tmp/$(id -u)-runtime-dir
-	if ! test -d "${XDG_RUNTIME_DIR}"; then
-		mkdir "${XDG_RUNTIME_DIR}"
-		chmod 0700 "${XDG_RUNTIME_DIR}"
-	fi
 
-	export LD_PRELOAD=/usr/lib/libwayland-server.so.0
-	export QT_QPA_PLATFORM=eglfs
-	export QT_QUICK_BACKEND=software
-
-	/usr/sbin/ls-hubd --conf /etc/luna-service2/ls-private.conf &
-	sleep 1
-	/usr/sbin/ls-hubd --public --conf /etc/luna-service2/ls-public.conf &
-	sleep 1
-	luna-prefs-service -d &
-	sleep 1
-	LunaSysMgr -l debug &
-	sleep 1
-	LunaSysService -l debug &
-	sleep 1
-	LunaAppManager -t -c -u luna &
-	sleep 1
-	luna-next
-fi
+/usr/sbin/ls-hubd --conf /etc/luna-service2/ls-private.conf &
+sleep 1
+/usr/sbin/ls-hubd --public --conf /etc/luna-service2/ls-public.conf &
+sleep 1
+MOJODB_ENGINE=leveldb mojodb-luna -c /etc/palm/db8/maindb.conf&
+MOJODB_ENGINE=leveldb mojodb-luna -c /etc/palm/db8/mediadb.conf&
+MOJODB_ENGINE=leveldb mojodb-luna -c /etc/palm/db8/tempdb.conf&
+sleep 1
+luna-prefs-service -d &
+sleep 1
+activitymanager &
+sleep 1
+LunaSysMgr -l debug &
+sleep 1
+LunaSysService -l debug &
+sleep 1
+export QT_QPA_PLATFORM=eglfs
+export QT_QPA_EGLFS_DISABLE_INPUT=1
+luna-next -plugin evdevmouse:/dev/input/event3 -plugin evdevkeyboard:/dev/input/event1 &
+sleep 1
+export QT_QPA_PLATFORM=wayland
+LunaAppManager -t -c -u luna &
+sleep 1
+LunaWebAppManager --verbose --allow-file-access-from-files
